703c785d18277ffe89ad249690f6cb649370bc6a
Eason Lu <ylu23@syr.edu>
2016-03-11 11:05:01 -0500

    finish stat

---
+++ stat.js
@@ -0,0 +1,8 @@
+var Redis = require('ioredis');
+var redis = Redis();
+
+module.exports = function(req, res, next){
+    redis.incr('stat#'+req.method+'#'+req.baseUrl + req.path);
+    next();    
+};
+
